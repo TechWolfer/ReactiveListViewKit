61bfaa1aa03b68e82c5fdb5bfbdf09c7908671b3
Cheng Zhang <showt2me@gmail.com>
2017-11-05 21:07:26 -0800

    Testing hotUserCellView user action

--- ReactiveListViewKitDemo/ReactiveListViewKitDemo/UI Layer/FeedList/FeedListViewModel.swift
+++ ReactiveListViewKitDemo/ReactiveListViewKitDemo/UI Layer/FeedList/FeedListViewModel.swift
@@ -38,35 +38,38 @@ class FeedListViewModel: NSObject, CopyableState {
             var sectionModels = [CZSectionModel]()
             
             // 1. HotUsers section
-            let HotUsersFeedModels = self.storyUsers.flatMap { CZFeedModel(viewClass: HotUserCellView.self,
-                                                                           viewModel: HotUserCellViewModel($0)) }
-            
-            let hotUsersSectionModel = CZSectionModel(isHorizontal: true,
-                                                      heightForHorizontal: HotUserSection.heightForHorizontal,
-                                                      feedModels: HotUsersFeedModels,
-                                                      headerModel: CZFeedListSupplementaryTextFeedModel(title: "Stories",
-                                                                                                        inset: UIEdgeInsets(top: 8,
-                                                                                                                            left: 10,
-                                                                                                                            bottom: 1,
-                                                                                                                            right: 10)),
-                                                      footerModel: CZFeedListSupplementaryLineFeedModel(),
-                                                      sectionInset: UIEdgeInsets(top: 0,
-                                                                                 left: 3,
-                                                                                 bottom: 0,
-                                                                                 right: 5))
-            sectionModels.append(hotUsersSectionModel)
+//            let HotUsersFeedModels = self.storyUsers.flatMap { CZFeedModel(viewClass: HotUserCellView.self,
+//                                                                           viewModel: HotUserCellViewModel($0)) }
+//            
+//            let hotUsersSectionModel = CZSectionModel(isHorizontal: true,
+//                                                      heightForHorizontal: HotUserSection.heightForHorizontal,
+//                                                      feedModels: HotUsersFeedModels,
+//                                                      headerModel: CZFeedListSupplementaryTextFeedModel(title: "Stories",
+//                                                                                                        inset: UIEdgeInsets(top: 8,
+//                                                                                                                            left: 10,
+//                                                                                                                            bottom: 1,
+//                                                                                                                            right: 10)),
+//                                                      footerModel: CZFeedListSupplementaryLineFeedModel(),
+//                                                      sectionInset: UIEdgeInsets(top: 0,
+//                                                                                 left: 3,
+//                                                                                 bottom: 0,
+//                                                                                 right: 5))
+//            sectionModels.append(hotUsersSectionModel)
             
             // 2. Feeds section
-            var feedModels = feeds.flatMap { CZFeedModel(viewClass: FeedCellView.self,
-                                                         viewModel: FeedCellViewModel($0)) }
+            var feedModels = self.suggestedUsers.flatMap { CZFeedModel(viewClass: HotUserCellCardView.self,
+                                                         viewModel: HotUserCellViewModel($0)) }
+            
+//            var feedModels = feeds.flatMap { CZFeedModel(viewClass: FeedCellView.self,
+//                                                         viewModel: FeedCellViewModel($0)) }
             
             // 3. SuggestedUsers - CellViewController
-            if feedModels.count > 0 {
-                let filteredSuggestedUsers = self.suggestedUsers
-                let suggestedUsersFeedModel = CZFeedModel(viewClass: HotUsersCellViewController.self,
-                                                          viewModel: HotUsersCellViewModel(filteredSuggestedUsers))
-                feedModels.insert(suggestedUsersFeedModel, at: 3)
-            }
+//            if feedModels.count > 0 {
+//                let filteredSuggestedUsers = self.suggestedUsers
+//                let suggestedUsersFeedModel = CZFeedModel(viewClass: HotUsersCellViewController.self,
+//                                                          viewModel: HotUsersCellViewModel(filteredSuggestedUsers))
+//                feedModels.insert(suggestedUsersFeedModel, at: 3)
+//            }
             let feedsSectionModel = CZSectionModel(feedModels: feedModels)
             
             sectionModels.append(feedsSectionModel)
